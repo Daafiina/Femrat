45257d35fc746f6f1a4646b6fbe2a8bdc797ec93
Dafina2001 <da52811@ubt-uni.net>
2024-03-04 07:14:46 -1100

    Scrolling functionality for navbar added

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,17 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Navbar.css'
 import logo from '../../assets/logo.png'
 
 
 const Navbar = () => {
+
+  const [scrolling,setScrollig] =useState(false);
+
+  useEffect(() => {
+    window.addEventListener('scroll',() => {
+      window.scrollY > 700 ? setScrollig(true) : setScrollig(false);
+    })
+  },[])
+
   return (
-    <nav>
+    //if sticky is true it returns dark-nav otherwise empty string
+    <nav className={`${scrolling ? 'dark-nav' : ''}`}> 
         <img src={logo} alt="Logo" className='logo' />
         <ul>
             <li>Home</li>
-            <li>Program</li>
             <li>About us</li>
             <li>Gallery</li>
+            <li>Program</li>
             <li><button className='btn color-1'>Contact us</button></li>
         </ul>
     </nav>
