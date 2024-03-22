7ea9c8e0a9b20798013e16295243d4bf180f43d9
Dafina2001 <da52811@ubt-uni.net>
2024-03-21 15:03:04 -1100

    Added media Query for Navabar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import './Navbar.css'
 import logo from '../../assets/logo.png'
+import menu from '../../assets/menu-icon.png'
+import { Link } from 'react-scroll';
 
 
 const Navbar = () => {
@@ -13,17 +15,25 @@ const Navbar = () => {
     })
   },[])
 
+  const [mobileMenu,setMobileMenu] = useState(false);
+
+  const toggleMenu = () => {
+    mobileMenu? setMobileMenu(false) : setMobileMenu(true);//When its false and you click it,it becomes true so the menu will be shown
+  }
+
   return (
     //if sticky is true it returns dark-nav otherwise empty string
     <nav className={`${scrolling ? 'dark-nav' : ''}`}> 
         <img src={logo} alt="Logo" className='logo' />
-        <ul>
-            <li>Home</li>
-            <li>About us</li>
-            <li>Gallery</li>
-            <li>Quotes</li>
-            <li><button className='btn color-1'>Contact us</button></li>
+        <ul className={mobileMenu? '' : 'hide-menu'}>
+            <li><Link to='home' smooth={true} offset={0} duration={500}>Home</Link></li>
+            <li><Link to='about' smooth={true} offset={-255} duration={500}>About us</Link></li>
+            <li><Link to='quotes' smooth={true} offset={-255} duration={500}>Quotes</Link></li>
+            <li><Link to='gallery' smooth={true} offset={-150} duration={500}>Gallery</Link></li>
+            <li><Link to='contact' smooth={true} offset={-265} duration={500} >Contact us</Link></li>
         </ul>
+        <img src={menu} alt="" className='menu-icon'
+        onClick={toggleMenu}/>
     </nav>
   )
 }
