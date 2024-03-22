cc7f7dffd2b5fc1f4082f0218850f9dc2ad600eb
Dafina2001 <da52811@ubt-uni.net>
2024-03-22 03:39:14 -1100

    Added media Query for Home section

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Home.css'
+import { Link } from 'react-scroll'
 
 const Home = () => {
   return (
@@ -7,7 +8,11 @@ const Home = () => {
       <div className='content'>
         <h1>We are the global champion for gender equality.</h1>
         <p>Women's rights are the fundamental human rights that were enshrined by the United Nations for every human being on the planet nearly 70 years ago.</p>
-        <button className='btn color-1'>Learn more!</button>
+        <button className='btn'>
+          <Link to='about' smooth={true} offset={-255} duration={500} style={{ textDecoration: 'none', color: 'inherit' }}>
+            Learn more!
+          </Link>
+        </button>
       </div>
       
     </div>
